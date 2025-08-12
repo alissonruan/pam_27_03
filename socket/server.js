f9295aa35c5bf2566184ae320e53069cdd9f96a0
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-08-11 22:08:35 -0300

    Funcionalidade Socket no Aplicativo

---
+++ socket/server.js
@@ -0,0 +1,25 @@
+const { DefaultTheme } = require("@react-navigation/native");
+const express = require("express");
+const http = require("http");
+const {Server}= require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket)=>{
+    console.log('um cliente se conectou')
+    socket.on('join_room', (room)=>{
+        socket.join(room);
+        console.log(`cliente entrou no canal: ${room}`)
+    });
+    socket.on('send_menssage', ({room, message})=>{
+        io.to(room).emit('recive_menssage', {room, msg: message})
+    });
+    socket.on('disconnect', ()=>{
+        console.log('um cliente se desconectou')
+    });
+})
+server.listen(3000, ()=>{
+    console.log('Servidor rodando na porta 3000');
+})
